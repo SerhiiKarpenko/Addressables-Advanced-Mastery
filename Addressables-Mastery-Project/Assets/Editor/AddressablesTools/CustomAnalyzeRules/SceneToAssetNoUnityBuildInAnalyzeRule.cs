b4d00916684d4606efd7e1f640b9d88bb3b55636
Serhii Karpenko <80330854+SerhiiKarpenko@users.noreply.github.com>
2024-03-27 23:35:23 +0100

    Added custom analyze rule
    This custom rule will hide all unity_builtin_extra problems

---
+++ Addressables-Mastery-Project/Assets/Editor/AddressablesTools/CustomAnalyzeRules/SceneToAssetNoUnityBuildInAnalyzeRule.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.AddressableAssets.Build;
+using UnityEditor.AddressableAssets.Build.AnalyzeRules;
+using UnityEditor.AddressableAssets.Settings;
+
+namespace Editor.AddressablesTools.CustomAnalyzeRules
+{
+  // Custom Rule For Analyzer
+  public class SceneToAssetNoUnityBuildInAnalyzeRule : CheckSceneDupeDependencies
+  {
+    public override string ruleName => "Scene To Addressable No Unity Build In";
+
+    public override List<AnalyzeResult> RefreshAnalysis(AddressableAssetSettings settings)
+    {
+      // result name is not a name, its a big string with a lot of information's
+      List<AnalyzeResult> results = base.RefreshAnalysis(settings)
+        .Where(x=>!x.resultName.Contains("unity_builtin_extra"))
+        .ToList();
+      
+      return results;
+    }
+  }
+
+  [InitializeOnLoad]
+  class RegisterSceneToAssetNoUnityBuildInAnalyzeRule
+  {
+    static RegisterSceneToAssetNoUnityBuildInAnalyzeRule()
+    {
+      AnalyzeSystem.RegisterNewRule<SceneToAssetNoUnityBuildInAnalyzeRule>();
+    }
+  }
+}
